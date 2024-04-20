b75f3d825d85598ddbc7f60106958216f456a88e
Gabriel <gabriel.gonzalezl@outlook.com>
2024-04-19 23:43:39 -0400

    new files

---
+++ Codigos Semestre 3/invertirListas.java
@@ -0,0 +1,15 @@
+public class invertirListas {
+    class LinkedList {
+
+        class Node {
+            int value;
+            Node next;
+        }
+
+        Node head;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
