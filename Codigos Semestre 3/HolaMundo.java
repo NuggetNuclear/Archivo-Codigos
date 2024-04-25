675f86dcc83a05e981064cb5bd3b0417930b613d
Gabriel <gabriel.gonzalezl@outlook.com>
2024-04-25 09:29:55 -0400

    Elimine archivos inutiles

--- Codigos Semestre 3/HolaMundo.java
+++
@@ -1,7 +0,0 @@
-public class HolaMundo { // Definición de clase, debe ser igual al nombre del archivo
-
-    public static void main(String[] args) { // Main o función principal
-        System.out.println("Hola mundo");
-    }
-
-}
