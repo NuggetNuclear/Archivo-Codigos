6b644356652707a9fd72e2eac6aa4ab711b78d4e
Gabriel <gabriel.gonzalezl@outlook.com>
2024-04-29 23:41:59 -0400

    Añadir y remover de playlist

--- Codigos Semestre 3/Spotify.java
+++ Codigos Semestre 3/Spotify.java
@@ -1,7 +1,5 @@
 import java.util.LinkedList;
-import java.util.Stack;
 import java.util.Queue;
-
 
 public class Spotify {
     public static class Cancion {
@@ -14,112 +12,136 @@ public class Spotify {
 
         public
 
-        Cancion(String nombre, String artista, String album, int duracion)
-        {
+        Cancion(String nombre, String artista, String album, int duracion) {
             this.nombre = nombre;
             this.artista = artista;
             this.album = album;
             this.duracion = duracion;
         }
 
-        String getNombre()
-        {
+        String getNombre() {
             return nombre;
         }
 
-        String getArtista()
-        {
+        String getArtista() {
             return artista;
         }
 
-        String getAlbum()
-        {
+        String getAlbum() {
             return album;
         }
 
-        int getDuracion()
-        {
+        int getDuracion() {
             return duracion;
         }
 
-        void getData()
-        {
-            System.out.println("Nombre: " + getNombre() + " | artistaa: " + getArtista() + " | Album: " + getAlbum() + " | Duración: " + (float)getDuracion()/60 + " minutos");
+        void getData() {
+            System.out.println("Nombre: " + getNombre() + " | artistaa: " + getArtista() + " | Album: " + getAlbum()
+                    + " | Duración: " + (float) getDuracion() / 60 + " minutos");
         }
     }
 
     public static class Playlist {
         private
-        
+
         String nombre;
-        java.util.LinkedList <Cancion> Cancions = new java.util.LinkedList<>();
+        java.util.LinkedList<Cancion> Canciones = new java.util.LinkedList<>();
         Cancion[] PlaylistArray = new Cancion[10];
         int espacioDisponible, currentIndex;
+        boolean isolatedSpace;
 
         public
 
-        Playlist(String nombre)
-        {
+        Playlist(String nombre) {
             this.nombre = nombre;
 
             espacioDisponible = 10;
             currentIndex = 0;
 
-            for(int i = 0; i < 10; i++)
-            {
+            for (int i = 0; i < PlaylistArray.length; i++) {
                 PlaylistArray = null;
             }
         }
 
-        String getNombre()
-        {
+        String getNombre() {
             return nombre;
         }
 
-        void addtoPlaylist(Cancion cancion)
-        {
-            Cancions.addLast(cancion);
+        void addtoPlaylist(Cancion cancion) {
+            Canciones.addLast(cancion);
+            
+            // Para despues: Añadir una forma de sincronizar las listas
         }
 
-        void addtoPlaylistArray(Cancion cancion)
-        {
-            if(espacioDisponible > 1)
-            {
+        void addtoPlaylistArray(Cancion cancion) {
+            if (isolatedSpace) {
+
+                PlaylistArray[currentIndex] = cancion;
+                currentIndex++;
+                isolatedSpace = false;
+
+            } else if (espacioDisponible > 1) {
+
                 PlaylistArray[currentIndex] = cancion;
                 currentIndex++;
                 espacioDisponible--;
+
             } else {
 
                 Queue<Cancion> canciones = new LinkedList<>(); // Crea una nueva cola de canciones
 
-                for(int i = 0; i < PlaylistArray.length - 1; i++) // Copia el arreglo a la cola
+                for (int i = 0; i < PlaylistArray.length - 1; i++) // Copia el arreglo a la cola
                 {
                     canciones.add(PlaylistArray[i]);
                 }
 
                 int newSize = (PlaylistArray.length) + 10; // Calcula el nuevo tamaño del arreglo
 
-                espacioDisponible = newSize - PlaylistArray.length; // Calcula el nuevo espacio disponible
+                espacioDisponible = PlaylistArray.length - currentIndex; // Calcula el nuevo espacio disponible
 
-                Cancion[] PlaylistArray = new Cancion[newSize]; // Crea el arreglo nuevo, sobreescribiendo el anterior
-                
-                for(int i = 0; i < newSize; i++) // Inicializa el nuevo arreglo en null
+                PlaylistArray = new Cancion[newSize]; // Crea el arreglo nuevo, sobreescribiendo el anterior
+
+                for (int i = 0; i < PlaylistArray.length; i++) // Inicializa el nuevo arreglo en null
                 {
                     PlaylistArray = null;
                 }
 
-                for(int i = 0; !canciones.isEmpty(); i++) // Copia los datos de la cola al arreglo nuevo
-                {                    
+                for (int i = 0; !canciones.isEmpty(); i++) // Copia los datos de la cola al arreglo nuevo
+                {
                     PlaylistArray[i] = canciones.poll();
                 }
 
-                PlaylistArray[currentIndex] = cancion;
-                currentIndex++;
+                PlaylistArray[currentIndex] = cancion; // Añade la nueva canción al arreglo nuevo
+
+                currentIndex++; // Actualiza el indice del próximo espacio vacío
             }
+
+            // Para despues: Añadir una forma de sincronizar las listas
         }
 
+        void removefromPlaylist(Cancion cancion) {
 
+            Canciones.remove(cancion);
+
+        }
+
+        void removefromPlaylistArray(Cancion cancion) {
+
+            for (int i = 0; i < currentIndex; i++) {
+
+                if (cancion == PlaylistArray[i]) {
+
+                    PlaylistArray[i] = null;
+                    isolatedSpace = true;
+
+                } else {
+
+                    System.err.println("No se encotró la canción en el arreglo");
+                }
+            }
+        }
     }
+
     public static void main(String[] args) {
     }
 }
